5823ab778ba02ca29f8dc216ad1d607d10b01b8d
Timi Ombe <timmyombe@gmail.com>
2023-07-14 05:00:05 +0100

    added code for module 13 (Networkk Programming

---
+++ PY4E/README.md
@@ -0,0 +1,4 @@
+# Python for Everybody
+[link to course](https://www.py4e.com)
+
+### Loading...
---
+++ PY4E/exercises/followingLinksWithBSoup.py
@@ -0,0 +1,75 @@
+# Network Programming
+
+"""
+Following Links in Python
+
+In this assignment you will write a Python program that expands on http://www.py4e.com/code3/urllinks.py. The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat the process a number of times and report the last name you find.
+
+We provide two files for this assignment. One is a sample file where we give you the name for your testing and the other is the actual data you need to process for the assignment
+
+Sample problem: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
+Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+Last name in sequence: Anayah
+Actual problem: Start at: http://py4e-data.dr-chuck.net/known_by_Arthur.html
+Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
+Hint: The first character of the name of the last page that you will load is: B
+Strategy
+The web pages tweak the height between the links and hide the page after a few seconds to make it difficult for you to do the assignment without writing a Python program. But frankly with a little effort and patience you can overcome these attempts to make it a little harder to complete the assignment without writing a Python program. But that is not the point. The point is to write a clever Python program to solve the program.
+
+Sample execution
+
+Here is a sample execution of a solution:
+
+$ python3 solution.py
+Enter URL: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Enter count: 4
+Enter position: 3
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Montgomery.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Mhairade.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Butchi.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Anayah.html
+The answer to the assignment for this execution is "Anayah".
+"""
+
+# Fix for 'collections has no attribute 'Callable'
+import collections
+import collections.abc
+collections.Callable = collections.abc.Callable
+
+# Imports
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+if len(url) < 1:
+    url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
+    # sample problem url
+
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
+
+# convert position to 0 scale (0,1,2...)
+position = position - 1
+
+while True:
+    if count < 0:
+        break
+    print(f"Retrieving: {url}")
+    html = urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Retrieving all of the anchor tags and updating the url
+    tags = soup('a')
+    url = tags[position].get('href', None)
+
+    # Updating the count value
+    count -= 1
+
